ec65c9a511954e1b89162dce77f6fd84bc6e66e2
steven nevins <steven.nevins.js@gmail.com>
2024-03-03 17:11:06 -0500

    feat: simpler forwarder

--- src/Forwarder.sol
+++ src/Forwarder.sol
@@ -1,94 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC2771Forwarder} from "@openzeppelin/metatx/ERC2771Forwarder.sol";
 import {ERC2771Context} from "@openzeppelin/metatx/ERC2771Context.sol";
-import {Address} from "@openzeppelin/utils/Address.sol";
 import {ECDSA} from "@openzeppelin/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/utils/cryptography/EIP712.sol";
 import {LibZip} from "solady/utils/LibZip.sol";
 
-contract Forwarder is ERC2771Forwarder {
+contract Forwarder is EIP712 {
     using LibZip for bytes;
     using ECDSA for bytes32;
 
-    constructor() ERC2771Forwarder("Forwader") {}
-
-    function executeBatch(bytes calldata compressedData) external payable {
-        bytes memory data = compressedData.flzDecompress();
-        (ForwardRequestData[] memory requests, address payable refundReceiver) = abi.decode(
-            data,
-            (ForwardRequestData[], address)
-        );
-
-        bool atomic = refundReceiver == address(0);
-        uint256 requestsValue;
-        uint256 refundValue;
-
-        for (uint256 i; i < requests.length; ++i) {
-            requestsValue += requests[i].value;
-            bool success = _executeMem(requests[i], atomic);
-            if (!success) {
-                refundValue += requests[i].value;
-            }
-        }
-
-        // The batch should revert if there's a mismatched msg.value provided
-        // to avoid request value tampering
-        if (requestsValue != msg.value) {
-            revert ERC2771ForwarderMismatchedValue(requestsValue, msg.value);
-        }
-
-        // Some requests with value were invalid (possibly due to frontrunning).
-        // To avoid leaving ETH in the contract this value is refunded.
-        if (refundValue != 0) {
-            // We know refundReceiver != address(0) && requestsValue == msg.value
-            // meaning we can ensure refundValue is not taken from the original contract's balance
-            // and refundReceiver is a known account.
-            Address.sendValue(refundReceiver, refundValue);
-        }
+    struct ForwardRequest {
+        address from;
+        address to;
+        uint256 value;
+        uint256 gas;
+        uint256 nonce;
+        bytes data;
     }
+
+    mapping(address => uint256) public nonces;
+
+    address private constant DRY_RUN_ADDRESS = 0x0000000000000000000000000000000000000000;
+
+    bytes32 private constant _TYPEHASH =
+        keccak256("ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)");
+
+    error InvalidRequest();
+
+    error InvalidSignature();
+
+    constructor() EIP712("Forwarder", "1") {}
 
     function execute(bytes calldata compressedData) external payable {
         bytes memory data = compressedData.flzDecompress();
 
-        ForwardRequestData memory request = abi.decode(data, (ForwardRequestData));
-        _executeMem(request, true);
+        (ForwardRequest memory request, bytes memory sig) = abi.decode(data, (ForwardRequest, bytes));
+        _execute(request, sig);
     }
-    function _validateMem(
-        ForwardRequestData memory request
-    ) internal view returns (bool isTrustedForwarder, bool active, bool signerMatch, address signer) {
-        (bool isValid, address recovered) = _recoverForwardRequestSignerMem(request);
 
-        return (
-            _isTrustedByTargetLocal(request.to),
-            request.deadline >= block.timestamp,
-            isValid && recovered == request.from,
-            recovered
+    function _execute(ForwardRequest memory req, bytes memory signature) internal returns (bool, bytes memory) {
+        if (!_verify(req, signature)) revert InvalidRequest();
+        nonces[req.from] = req.nonce + 1;
+
+        (bool success, bytes memory returndata) = req.to.call{gas: req.gas, value: req.value}(
+            abi.encodePacked(req.data, req.from)
         );
+
+        // Validate that the relayer has sent enough gas for the call.
+        // See https://ronan.eth.limo/blog/ethereum-gas-dangers/
+        if (gasleft() <= req.gas / 63) {
+            // We explicitly trigger invalid opcode to consume all gas and bubble-up the effects, since
+            // neither revert or assert consume all gas since Solidity 0.8.0
+            // https://docs.soliditylang.org/en/v0.8.0/control-structures.html#panic-via-assert-and-error-via-require
+            /// @solidity memory-safe-assembly
+            assembly {
+                invalid()
+            }
+        }
+
+        return (success, returndata);
     }
-
-    function _recoverForwardRequestSignerMem(
-        ForwardRequestData memory request
-    ) internal view virtual returns (bool, address) {
-        (address recovered, ECDSA.RecoverError err, ) = _hashTypedDataV4(
-            keccak256(
-                abi.encode(
-                    _FORWARD_REQUEST_TYPEHASH,
-                    request.from,
-                    request.to,
-                    request.value,
-                    request.gas,
-                    nonces(request.from),
-                    request.deadline,
-                    keccak256(request.data)
-                )
-            )
-        ).tryRecover(request.signature);
-
-        return (err == ECDSA.RecoverError.NoError, recovered);
-    }
-
-    function _isTrustedByTargetLocal(address target) internal view returns (bool) {
+    function _isTrustedByTarget(address target) internal view returns (bool) {
         bytes memory encodedParams = abi.encodeCall(ERC2771Context.isTrustedForwarder, (address(this)));
 
         bool success;
@@ -109,57 +82,12 @@ contract Forwarder is ERC2771Forwarder {
         return success && returnSize >= 0x20 && returnValue > 0;
     }
 
-    function _executeMem(
-        ForwardRequestData memory request,
-        bool requireValidRequest
-    ) internal virtual returns (bool success) {
-        (bool isTrustedForwarder, bool active, bool signerMatch, address signer) = _validateMem(request);
+    function _verify(ForwardRequest memory req, bytes memory signature) internal view returns (bool) {
+        address signer = _hashTypedDataV4(
+            keccak256(abi.encode(_TYPEHASH, req.from, req.to, req.value, req.gas, req.nonce, keccak256(req.data)))
+        ).recover(signature);
+        if (!(tx.origin == DRY_RUN_ADDRESS || signer == req.from)) revert InvalidSignature();
 
-        // Need to explicitly specify if a revert is required since non-reverting is default for
-        // batches and reversion is opt-in since it could be useful in some scenarios
-        if (requireValidRequest) {
-            if (!isTrustedForwarder) {
-                revert ERC2771UntrustfulTarget(request.to, address(this));
-            }
-
-            if (!active) {
-                revert ERC2771ForwarderExpiredRequest(request.deadline);
-            }
-
-            if (!signerMatch) {
-                revert ERC2771ForwarderInvalidSigner(signer, request.from);
-            }
-        }
-
-        // Ignore an invalid request because requireValidRequest = false
-        if (isTrustedForwarder && signerMatch && active) {
-            // Nonce should be used before the call to prevent reusing by reentrancy
-            uint256 currentNonce = _useNonce(signer);
-
-            uint256 reqGas = request.gas;
-            address to = request.to;
-            uint256 value = request.value;
-            bytes memory data = abi.encodePacked(request.data, request.from);
-
-            uint256 gasLeft;
-
-            assembly {
-                success := call(reqGas, to, value, add(data, 0x20), mload(data), 0, 0)
-                gasLeft := gas()
-            }
-
-            _checkGasForwarded(gasLeft, request);
-
-            emit ExecutedForwardRequest(signer, currentNonce, success);
-        }
-    }
-
-    function _checkGasForwarded(uint256 gasLeft, ForwardRequestData memory request) internal pure {
-        if (gasLeft < request.gas / 63) {
-            /// @solidity memory-safe-assembly
-            assembly {
-                invalid()
-            }
-        }
+        return nonces[req.from] == req.nonce && _isTrustedByTarget(req.to);
     }
 }
